894cc42d2f70dd58b0f4d9e271169188fb6c5f8b
AbdullahBendary <abdullahbendary7@gmail.com>
2024-08-21 05:47:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/islami/ui/HomeFragment.kt
@@ -0,0 +1,17 @@
+package com.example.islami.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.islami.R
+import com.example.islami.base.BaseFragment
+import com.example.islami.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+    override fun onClicks() {
+        TODO("Not yet implemented")
+    }
+
+}
