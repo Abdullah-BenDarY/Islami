5e480a9bcb8993e0c4f983d0520592a908ce85bb
AbdullahBendary <abdullahbendary7@gmail.com>
2024-08-22 03:34:53 +0300

    feat: - implement home activity design & code - implement navigation component

--- app/src/main/java/com/example/islami/base/BaseFragment.kt
+++ app/src/main/java/com/example/islami/base/BaseFragment.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment <VB : ViewBinding>(private val bindingInflater : (inflater: LayoutInflater) -> VB ) : Fragment() {
+abstract class BaseFragment <VB :
+ViewBinding>(private val bindingInflater : (inflater: LayoutInflater) -> VB ) : Fragment() {
+
     private var _binding: VB? = null
-    val binding :VB get() = _binding as VB
+    protected val binding :VB get() = _binding as VB
 
     override fun onCreateView(
         inflater: LayoutInflater,
---
+++ app/src/main/java/com/example/islami/ui/QuranFrafment.kt
@@ -0,0 +1,11 @@
+package com.example.islami.ui
+
+import com.example.islami.base.BaseFragment
+import com.example.islami.databinding.FragmentQuranBinding
+
+class QuranFrafment : BaseFragment<FragmentQuranBinding>(FragmentQuranBinding::inflate) {
+    override fun onClicks() {
+
+    }
+
+}
