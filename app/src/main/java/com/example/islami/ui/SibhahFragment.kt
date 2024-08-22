57d023cde6e3987badabdd9af5879c60f9b854a5
AbdullahBendary <abdullahbendary7@gmail.com>
2024-08-22 03:40:14 +0300

    feat: - implement SibhahFragment design

---
+++ app/src/main/java/com/example/islami/ui/SibhahFragment.kt
@@ -0,0 +1,11 @@
+package com.example.islami.ui
+
+
+import com.example.islami.base.BaseFragment
+import com.example.islami.databinding.FragmentSibhahBinding
+
+class SibhahFragment : BaseFragment<FragmentSibhahBinding>(FragmentSibhahBinding::inflate) {
+    override fun onClicks() {
+
+    }
+}
